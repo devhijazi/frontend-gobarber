95b89b371bfaf10a2920ea03ff5e7b944a9628b7
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-09-15 19:08:58 -0300

    chore: creating toast files with animations

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './Toast';
+
+import { ToastMessage } from '../../hooks/toast';
+
+import { Container } from './styles';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const Transitions = useTransition(messages, message => message.id, {
+    from: { right: '-120%', opacity: 0 },
+    enter: { right: '0%', opacity: 1 },
+    leave: { right: '-120%', opacity: 0 },
+  });
+  return (
+    <Container>
+      {Transitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default ToastContainer;
